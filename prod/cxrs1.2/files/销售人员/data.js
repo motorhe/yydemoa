66fe28346bebf57159077f3b88d0caca51f1f1c8
motorhe <motorhe@163.com>
2020-01-09 12:19:26 +0800

    202001091

---
+++ prod/cxrs1.2/files/销售人员/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o,p,q,r],s,_(t,u,v,w,x,y,z,_(),A,_(B,C,D,E,F,_(G,H,I,J),K,null,L,M,N,M,O,P,Q,null,R,S,T,U,V,W,X,S),Y,_(),Z,_(),ba,_(bb,[])),bc,_(),bd,_());}; 
+var b="url",c="销售人员.html",d="generationDate",e=new Date(1553134919253.15),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="tc",n="OnLoadVariable",o="sfz",p="sfz_n",q="login",r="Menu",s="page",t="packageId",u="e02d2ad40ba6432589fafb1f1ff2ead0",v="type",w="Axure:Page",x="name",y="销售人员",z="notes",A="style",B="baseStyle",C="627587b6038d43cca051c114ac41ad32",D="pageAlignment",E="center",F="fill",G="fillType",H="solid",I="color",J=0xFFF6F7F9,K="image",L="imageHorizontalAlignment",M="near",N="imageVerticalAlignment",O="imageRepeat",P="auto",Q="favicon",R="sketchFactor",S="0",T="colorStyle",U="appliedColor",V="fontName",W="Applied Font",X="borderWidth",Y="adaptiveStyles",Z="interactionMap",ba="diagram",bb="objects",bc="masters",bd="objectPaths";
+return _creator();
+})());
